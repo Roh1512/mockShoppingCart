146658f0c3b66a7f8b133a37479585883c4b85b3
Rohith19 <rohith1512a@gmail.com>
2024-02-14 22:44:17 +0530

    some tweaks

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,15 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "../header/header";
 import Footer from "../footer/Footer";
 import styles from "./Layout.module.css";
+import LoaderAnimation from "../loaderAnimation/Loader";
 
 function Layout() {
+  const navigation = useNavigation();
   return (
     <>
       <div className={styles.layoutDiv}>
         <Header />
         <main>
-          <Outlet />
+          {navigation.state === "loading" ? <LoaderAnimation /> : <Outlet />}
         </main>
         <Footer />
       </div>
