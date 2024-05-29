9f7241f49ac7ebc0639eb25c453de835233b1f13
Rohith19 <rohith1512a@gmail.com>
2024-05-29 22:12:45 +0530

    Year genrator for footer added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,11 @@
 import styles from "./Footer.module.css";
 function Footer() {
+  const date = new Date().getFullYear();
   return (
     <>
       <footer>
         <p>
-          <span>&#169;</span> Shopping Cart 2024
+          <span>&#169;</span> Shopping Cart {date}
         </p>
       </footer>
     </>
